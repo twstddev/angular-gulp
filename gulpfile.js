d594ee97449eb9c237961532b2f60c8acd0e0fff
twstddev <twstd.dev@gmail.com>
2014-06-09 20:50:48 +0100

    Add gulp development task

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,15 @@
 var gulp = require( "gulp" );
 
+var run_sequence = require( "run-sequence" );
+
+GLOBAL.plugins = require( "gulp-load-plugins" )();
+
+// load tasks from the tasks directory
+require( "gulp-load" )( gulp );
+gulp.loadTasks( __dirname );
+
+gulp.task( "development", function( done ) {
+	run_sequence(
+		done
+	);
+} );
