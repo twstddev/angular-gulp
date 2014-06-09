d8c0b46640a82c3b34e6250d3e5759ec3dce2f47
twstddev <twstd.dev@gmail.com>
2014-06-09 21:43:24 +0100

    Add compass task

---
+++ tasks/compass.js
@@ -0,0 +1,22 @@
+module.exports = function( gulp ) {
+	var process_sass = function() {
+		return gulp.src( get_application_path( "sass/main.scss" ) )
+			.pipe( plugins.compass( {
+				css : get_application_path( "css" ),
+				sass : get_application_path( "sass" ),
+				image : get_application_path( "img" ),
+				font : get_application_path( "fonts" ),
+				style : "expanded"
+			} ) )
+			.on( "error", output_error );
+	}; 
+
+	// development task
+	gulp.task( "compass:build", function() {
+		gulp.watch( get_application_path( "sass/**/*.scss" ), process_sass );
+		return process_sass();
+	} );
+
+	// production task
+	gulp.task( "compass:dist", process_sass );
+}
