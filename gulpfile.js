3bf37d219e0b1dc2592b6bdd92e9215ba625be31
twstddev <twstd.dev@gmail.com>
2014-06-09 21:20:29 +0100

    Add livereload server

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,37 @@
 var gulp = require( "gulp" );
-
+var path_helper = require( "path" );
 var run_sequence = require( "run-sequence" );
 
 GLOBAL.plugins = require( "gulp-load-plugins" )();
+
+// a config object that contains main configuration for
+// the application.
+GLOBAL.config = {
+	hostname : "localhost",
+	port : 3000,
+	base : "app",
+	dist : "dist"
+};
+
+/**
+ * @brief Converts given path to a relative to the application
+ * path.
+ *
+ * @return String converted path
+ */
+GLOBAL.get_application_path = function( path ) {
+	return path_helper.join( GLOBAL.config.base, path );
+}
+
+/**
+ * @brief Calms down thrown errors.
+ * Logs them instead of stopping gulp execution.
+ */
+GLOBAL.output_error = function( error ) {
+	console.log( error.message );
+}
+
+
 
 // load tasks from the tasks directory
 require( "gulp-load" )( gulp );
@@ -10,6 +39,7 @@ gulp.loadTasks( __dirname );
 
 gulp.task( "development", function( done ) {
 	run_sequence(
+		"livereload",
 		done
 	);
 } );
