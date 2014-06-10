214a0c8ce7e63b03dd6d04f8a91da22e7559d1ec
twstddev <twstd.dev@gmail.com>
2014-06-10 21:36:32 +0100

    latest changes

--- app/test/spec/modules/header/directives/header_spec.js
+++ app/test/spec/modules/header/directives/header_spec.js
@@ -1,9 +1,19 @@
 describe( "HeaderDirective", function() {
+	var $compile = null;
+	var $rootScope = null;
+
 	beforeEach( function( done ) {
-		console.log( done );
 		require( [ "angular-mocks" ], function() {
-			console.log( "suka" );
-			done();
+			require( [ "js/modules/header/main" ], function() {
+				module( "header" );
+
+				inject( function( _$compile_, _$rootScope_ ) {
+					$compile = _$compile_;
+					$rootScope = _$rootScope_;
+				} );
+
+				done();
+			} );
 		} );
 	} );
 
