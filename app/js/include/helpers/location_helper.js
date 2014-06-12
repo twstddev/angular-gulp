a4f806c8b1c991de5649fb22ecaeb1dbf1450132
twstddev <twstd.dev@gmail.com>
2014-06-12 12:51:18 +0100

    Complete location helper

--- app/js/include/helpers/location_helper.js
+++ app/js/include/helpers/location_helper.js
@@ -1,4 +1,15 @@
 define( [ "angular" ], function() {
+	// private scope
+	// pattern that extracts value between leading and trailing slash
+	var m_regex_pattern =  /^\/*(.*?)\/*$/;
+
+	/**
+	 * @brief Returns path without leading and trailing slashes.
+	 */
+	var m_process_path = function( path ) {
+		return path.match( m_regex_pattern )[ 1 ];
+	};
+
 	/**
 	 * @brief Helper that adds some extra functionality
 	 * for location and paths management and processing.
@@ -10,7 +21,7 @@ define( [ "angular" ], function() {
 	 * @brief Compares given paths.
 	 */
 	LocationHelper.prototype.comparePaths = function( current_path, compare_to_path ) {
-		return false;
+		return ( m_process_path( current_path ) === m_process_path( compare_to_path ) );
 	};
 
 	return [ "$location", function( $location ) {
